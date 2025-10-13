88030b6c03a1f44e91f986cefe743ed034a76837
ManuelMannhold <m.mannhold@gmx.de>
2025-10-13 11:57:42 +0200

    add links to chats / styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { ChatRoom } from './core/chat/chat-room/chat-room';
+import { PrivateChat } from './core/chat/private-chat/private-chat';
 
 
 export const routes: Routes = [
     { path: '', component: ChatRoom },
+    {path: '#chat', component: ChatRoom},
+    {path: '#private', component: PrivateChat}
 ];
