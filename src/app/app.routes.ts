e48028318dba23555105e1dfcda80b09840d3b48
ManuelMannhold <m.mannhold@gmx.de>
2025-10-16 11:40:24 +0200

    add active path by join

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,8 @@ import { Routes } from '@angular/router';
 import { ChatRoom } from './core/chat/chat-room/chat-room';
 import { PrivateChat } from './core/chat/private-chat/private-chat';
 
-
 export const routes: Routes = [
-    { path: '', component: ChatRoom },
-    {path: 'chat', component: ChatRoom},
-    {path: 'private', component: PrivateChat}
+  { path: '', redirectTo: '/chat', pathMatch: 'full' },
+  { path: 'chat', component: ChatRoom },
+  { path: 'private', component: PrivateChat },
 ];
