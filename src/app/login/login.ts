6c18f4f230c06fccdac9c232f38e69639c9fd1ca
ManuelMannhold <m.mannhold@gmx.de>
2025-12-04 11:54:18 +0100

    add login function

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
 export class Login {
+  username: string = '';
 
+  constructor(private router: Router) {}
+
+  submitName() {
+    if (!this.username.trim()) return;
+    this.router.navigate(['/toChat']);
+  }
 }
