87c6725345637f6fd52f1a1eb94333e75e91cff2
ManuelMannhold <M.Mannhold@gmx.de>
2025-12-15 16:40:10 +0100

    add new component change-name

---
+++ src/app/core/change-name/change-name.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-name',
+  imports: [],
+  templateUrl: './change-name.html',
+  styleUrl: './change-name.scss'
+})
+export class ChangeName {
+
+}
