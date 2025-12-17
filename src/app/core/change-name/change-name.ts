643f0f9d80a3f29ea2b26eb2dceba56b2eadc837
ManuelMannhold <m.mannhold@gmx.de>
2025-12-17 11:59:08 +0100

    add change-name function

--- src/app/core/change-name/change-name.ts
+++ src/app/core/change-name/change-name.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../../services/user';
 
 @Component({
   selector: 'app-change-name',
   imports: [],
   templateUrl: './change-name.html',
-  styleUrl: './change-name.scss'
+  styleUrl: './change-name.scss',
 })
 export class ChangeName {
-    newName: string = '';
+  constructor(private router: Router, private user: User) {}
+  newName: string = '';
 
   changeName(newName: string): void {
-    
     console.log('Name geändert!', newName);
+    this.user.username = newName;
+    this.user.saveToLocalStorage();
+    this.router.navigate(['/chat']);
   }
-  
 }
